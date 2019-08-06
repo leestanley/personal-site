50c236aa06959716e74ada3dc7316a704a59c347
leestanley <stanleylee91776@gmail.com>
2019-08-06 15:22:36 -0700

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+  import React, { Component } from 'react';
+  import { Icon } from 'antd';
+  import './App.css';
+  import face from './photo.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  class App extends Component {
+    render() {
+      return (
+        <div className="container">
+          <div className="content">
+            <img src={face} width="175" alt=""/>
+            <h1>Stanley Lee</h1>
+            <p>My website is still in progress, contact me below!</p>
+              <div id='icons'>
+                <a href="https://www.linkedin.com/in/leestanleysg/">
+                  <Icon type="linkedin"/>
+                </a>
+                <a href="https://github.com/leestanley">
+                  <Icon type="github"/>
+                </a>
+                <a href="mailto:stl005@ucsd.edu">
+                  <Icon type="mail"/>
+                </a>
+              </div>
+          </div>
+        </div>
+      );
+    }
+  }
 
-export default App;
+  export default App;

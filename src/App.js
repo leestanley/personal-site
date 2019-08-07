493badabfd54338c52bb992366deb48cd3ae2460
leestanley <stanleylee91776@gmail.com>
2019-08-06 22:17:59 -0700

    add animation

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
-  import React, { Component } from 'react';
-  import { Icon, Button } from 'antd';
-  import './App.css';
-  import face from './photo.png'
+import React, { Component } from "react";
+import { Icon, Button } from "antd";
+import QueueAnim from "rc-queue-anim";
+import "./App.css";
+import face from "./photo.png";
 
-  class App extends Component {
-    render() {
-      const resume = 'https://drive.google.com/file/d/1cEU7myTYPqcKQYlQvNRfn6wLqVFRTdLb/view'
-      return (
-        <div className="container">
-          <div className="content">
-            <img src={face} width="175" alt=""/>
+class App extends Component {
+  render() {
+    const resume =
+      "https://drive.google.com/file/d/1cEU7myTYPqcKQYlQvNRfn6wLqVFRTdLb/view";
+    return (
+      <div className="content">
+        <QueueAnim interval={200} duration={1000}>
+          <div key="1">
+            <img src={face} width="175" alt="" />
             <h1>Stanley Lee</h1>
-            <p>My website is still in progress, contact me below!</p>
-              <div id='icons'>
-                <a href="https://www.linkedin.com/in/leestanleysg/">
-                  <Icon type="linkedin"/>
-                </a>
-                <a href="https://github.com/leestanley">
-                  <Icon type="github"/>
-                </a>
-                <a href="mailto:stl005@ucsd.edu">
-                  <Icon type="mail"/>
-                </a>
-              </div>
-              <Button onClick={() => window.open(resume, "_blank")} className='buttons' type="primary" shape="round">Download Resume</Button>
           </div>
-        </div>
-      );
-    }
+          <div key="2">
+            <p>My website is still in progress, contact me below!</p>
+          </div>
+          <div id="icons" key="3">
+            <a key="1" href="https://www.linkedin.com/in/leestanleysg/">
+              <Icon type="linkedin" />
+            </a>
+            <a key="2" href="https://github.com/leestanley">
+              <Icon type="github" />
+            </a>
+            <a key="3" href="mailto:stl005@ucsd.edu">
+              <Icon type="mail" />
+            </a>
+          </div>
+          <Button
+            onClick={() => window.open(resume, "_blank")}
+            type="primary"
+            shape="round"
+            key="4"
+          >
+            Download Resume
+          </Button>
+        </QueueAnim>
+      </div>
+    );
   }
+}
 
-  export default App;
+export default App;

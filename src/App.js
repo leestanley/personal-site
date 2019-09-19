4e69c844efa55dc6d60180805e28c8dc0e24bfe9
leestanley <stanleylee91776@gmail.com>
2019-09-19 02:03:36 -0700

    added devpost

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React, { Component } from "react";
 import { Icon, Button } from "antd";
 import QueueAnim from "rc-queue-anim";
 import "./App.css";
-import face from "./photo.png";
+
+import face from "./assets/photo.png";
+
+const DevSvg = () => (
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 34 34"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M8.49996 2.83334L0.885376 17L8.49996 31.1667H25.5L33.2307 17L25.5 2.83334H8.49996ZM11.3333 9.91667L18.6107 9.928C20.5034 10.1929 24.0833 12.0034 24.0833 17C24.0833 21.9966 20.5034 23.8071 18.6107 24.0663L18.5115 24.0833H11.3333V9.91667ZM14.1666 12.75V21.25H18.2948C18.8147 21.1452 21.25 20.4638 21.25 17C21.25 13.5518 18.8487 12.8605 18.2891 12.75H14.1666Z"
+      fill="#1890FF"
+    />
+  </svg>
+);
+
+const DevpostIcon = props => <Icon component={DevSvg} {...props} />;
 
 class App extends Component {
   render() {
@@ -14,7 +32,7 @@ class App extends Component {
           <img src={face} alt="face" width="175" height="175" />
           <h1>Stanley Lee</h1>
         </div>
-        <QueueAnim delay ={200} interval={250} duration={1500}>
+        <QueueAnim delay={200} interval={250} duration={1500}>
           <div key="1">
             <p>My website is still in progress, contact me below!</p>
           </div>
@@ -24,6 +42,9 @@ class App extends Component {
             </a>
             <a href="https://github.com/leestanley">
               <Icon type="github" />
+            </a>
+            <a href="https://devpost.com/leestanley">
+              <DevpostIcon className='devpost' />
             </a>
             <a href="mailto:stl005@ucsd.edu">
               <Icon type="mail" />
@@ -38,7 +59,6 @@ class App extends Component {
             Download Resume
           </Button>
         </QueueAnim>
-
       </div>
     );
   }
